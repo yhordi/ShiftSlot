eaf73c1aef5b6a108b2df9d500dcfa92e95fdb8b
Jordan Kamin <jordanakamin@gmail.com>
2017-07-31 12:45:32 -0700

    adds shows to seeds.rb, builds date method untested

--- app/models/show.rb
+++ app/models/show.rb
@@ -2,4 +2,8 @@ class Show < ApplicationRecord
   belongs_to :venue
   validates_associated :venue
   validates_presence_of :start
+
+  def date
+    self.start.to_date
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,10 @@
-Venue.create(name: 'Victory Lounge', location: 'Seattle, WA')
-Venue.create(name: 'Black Lodge', location: 'Seattle, WA')
-Venue.create(name: 'Lucky Liquor', location: 'Tukwila, WA')
+Venue.create!(name: 'Victory Lounge', location: 'Seattle, WA')
+Venue.create!(name: 'Black Lodge', location: 'Seattle, WA')
+Venue.create!(name: 'Lucky Liquor', location: 'Tukwila, WA')
+20.times do
+  s = Show.new(doors: Faker::Date.forward)
+  s.start = s.doors + 30.minutes
+  s.show_end = s.doors + 4.hours
+  s.venue_id = [1,2,3].sample
+  s.save!
+end
