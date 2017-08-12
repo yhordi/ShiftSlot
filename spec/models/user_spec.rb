0a301604cb09e67e6c73aa274839ce60d609c852
Jordan Kamin <jordanakamin@gmail.com>
2017-08-11 17:01:24 -0700

    adds tests for user model

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,8 +1,27 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
+  let(:user) {FactoryGirl.create :user}
+  let(:job) {FactoryGirl.create :job}
   describe 'validations' do
     it { is_expected.to validate_presence_of :email }
     it { is_expected.to validate_length_of(:name).is_at_least(3) }
+  end
+
+  describe '#authorized?' do
+    it 'returns true if the user is authorized for a job' do
+      user.jobs << job
+      expect(user.authorized?(job)).to eq(true)
+    end
+    it 'returns false if the user is not authorized for a job' do
+      expect(user.authorized?(job)).to eq(false)
+    end
+
+  end
+  describe '#adjust_jobs' do
+    it 'authorizes a user for jobs based on a passed in array' do
+      user.adjust_jobs([job.id.to_s])
+      expect(user.jobs).to include(job)
+    end
   end
 end
