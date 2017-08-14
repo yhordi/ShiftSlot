08db2604d93576c3131b6536b29c7c26931a7a12
Jordan Kamin <jordanakamin@gmail.com>
2017-08-14 14:25:05 -0700

    builds .day color change handlers on click

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -1,17 +1,27 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
 $(document).ready(function(){
-  $('.day').on('click', (e)=>{
-    console.log(e.target)
-    determinePreferrence(e.target)
+  $('.day').on('click', (e) => {
+    determinePreferrence($(e.target));
   })
 })
 
-const determinePreferrence = (button) => {
+const determinePreferrence = ($button) => {
   // I'm trying to figure out how to determine if the button has been clicked
   // based on data available from the dom
-  debugger
-  if(button.dataset == undefined) {
-    $(button).data('prefferred', true)
+  if($button.data('preferred') == 'neutral') {
+    $button.data('preferred', true);
+    $button.removeClass('btn-default');
+    $button.addClass('btn-success');
+  } else if($button.data('preferred') == true) {
+    console.log*'yoyoyo'
+    $button.data('preferred', false);
+    $button.removeClass('btn-success');
+    $button.addClass('btn-danger');
+  } else {
+    $button.data('preferred', 'neutral')
+    $button.removeClass('btn-danger')
+    $button.addClass('btn-default');
+
   }
 }
