9dd9ff60025536b873f06c3e0040b22e68b2ce02
Jordan Kamin <jordanakamin@gmail.com>
2017-08-14 16:54:48 -0700

    builds form to send day preferences

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -2,6 +2,7 @@
 // All this logic will automatically be available in application.js.
 $(document).ready(function(){
   $('.day').on('click', (e) => {
+    e.preventDefault()
     determinePreferrence($(e.target));
   })
 })
@@ -9,18 +10,28 @@ $(document).ready(function(){
 const determinePreferrence = ($button) => {
   // I'm trying to figure out how to determine if the button has been clicked
   // based on data available from the dom
-  console.log($button.attr('data-preferred'))
   if($button.attr('data-preferred') == 'neutral') {
     $button.attr('data-preferred', true);
     $button.removeClass('btn-default');
     $button.addClass('btn-success');
+    addHidden($button)
   } else if($button.attr('data-preferred') == 'true') {
     $button.attr('data-preferred', false);
     $button.removeClass('btn-success');
     $button.addClass('btn-danger');
+    updateHidden('false', $button)
   } else {
     $button.attr('data-preferred', 'neutral')
     $button.removeClass('btn-danger')
     $button.addClass('btn-default');
+    $(`#${$button.attr('name')}Attr`).remove()
   }
+}
+
+const addHidden = ($button) => {
+  $('#new_preferred_day').prepend(`<input type='hidden' id=${$button.attr('name')}Attr name=days[${$button.attr('name')}] value=true/>`)
+}
+
+const updateHidden = (preference, $button) => {
+  $(`#${$button.attr('name')}Attr`).attr('value', 'false')
 }
---
+++ app/controllers/preferred_days_controller.rb
@@ -0,0 +1,10 @@
+class PreferredDaysController < ApplicationController
+
+  def create
+    p params
+  end
+
+  def update
+  end
+
+end
