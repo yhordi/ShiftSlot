1eaadded381718f77e22ac36a9b7e6268fa8027d
Jordan Kamin <jordanakamin@gmail.com>
2017-08-14 17:10:18 -0700

    WIP EOD building out contionally rendering preferred day form

--- app/controllers/preferred_days_controller.rb
+++ app/controllers/preferred_days_controller.rb
@@ -1,7 +1,15 @@
 class PreferredDaysController < ApplicationController
 
   def create
-    p params
+    params[:days].each do |day|
+      if params[:days][day] == 'false'
+        PreferredDay.create(name: day, preferred: false, user_id: params[:user_id])
+      else params[:days][day] == 'true/'
+        PreferredDay.create(name: day, preferred: true, user_id: params[:user_id])
+      end
+    end
+    flash[:notice] = 'days updated'
+    redirect_to edit_user_path(params[:user_id])
   end
 
   def update
