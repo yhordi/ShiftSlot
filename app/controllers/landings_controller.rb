578d7eb4d137419b8823db240f71d7b97f547887
Jordan Kamin <jordanakamin@gmail.com>
2017-08-30 13:52:40 -0700

    adds check for if user is logged in to set @shifts

--- app/controllers/landings_controller.rb
+++ app/controllers/landings_controller.rb
@@ -1,7 +1,9 @@
 class LandingsController < ApplicationController
   skip_before_action :require_login, only: :index
   def index
-    @shifts = Show.available_shifts_for(current_user)
+    if current_user
+      @shifts = Show.available_shifts_for(current_user)
+    end
     render 'index'
   end
 end
