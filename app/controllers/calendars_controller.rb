81f5b108047518f22885fafdaa00a3950563189c
Jordan Kamin <jordanakamin@gmail.com>
2017-09-18 15:43:56 -0700

    WIP hitting api

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -1,5 +1,12 @@
 class CalendarsController < ApplicationController
   def sync
-    
+    headers = "Bearer #{ENV['TOKEN']}"
+    url = "https://www.googleapis.com/calendar/v3/#{ENV['CAL_ID']}/events?key=#{ENV['CAL_KEY']}"
+    req = HTTParty.get(url, headers: {"Authorization" => headers})
+    p url
+    p "!"*50
+    p req
+    p "*"*50
+    p req.response
   end
 end
