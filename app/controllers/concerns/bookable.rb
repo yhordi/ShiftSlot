748e7a113f1bc44aa7eea19efba17c2e44005e3b
Jordan Kamin <jordanakamin@gmail.com>
2017-09-18 17:11:39 -0700

    writes Bookable#build

--- app/controllers/concerns/bookable.rb
+++ app/controllers/concerns/bookable.rb
@@ -1,3 +1,13 @@
+require 'date'
 module Bookable
   extend ActiveSupport::Concern
+
+  def build(response)
+    shows = []
+    response["items"].each do |item|
+      p item["start"]
+      shows << Show.new(start: DateTime.parse(item["start"]["dateTime"]), show_end: DateTime.parse(item["end"]["dateTime"]), info: item["summary"])
+    end
+    shows
+  end
 end
