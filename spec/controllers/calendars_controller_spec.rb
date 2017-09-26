4798aea24c83603723f99169fe5187b96a55488a
Jordan Kamin <jordanakamin@gmail.com>
2017-09-26 11:05:24 -0700

    addds test for assigns @google_shows

--- spec/controllers/calendars_controller_spec.rb
+++ spec/controllers/calendars_controller_spec.rb
@@ -1,10 +1,25 @@
 require 'rails_helper'
-
+require_relative '../support/response'
 RSpec.describe CalendarsController, type: :controller do
+  let(:user) { FactoryGirl.create(:user)}
   describe '#sync' do
-    it 'renders the sync template'
-    it 'responds with a 200'
-    it 'assigns @google_shows'
+    let(:resp_double) { double(parsed_response: fake_response)}
+    before(:each) do
+      sign_in user
+      allow(HTTParty).to receive(:get).and_return(resp_double)
+    end
+    it 'responds with a 200' do
+      get :sync, params: {token: 'HAM'}
+      expect(response.status).to eq(200)
+    end
+    it 'renders the sync template' do
+      get :sync, params: {token: 'HAM'}
+      expect(response).to render_template('calendars/sync')
+    end
+    it 'assigns @google_shows' do
+      get :sync, params: {token: 'HAM'}
+      expect(assigns[:google_shows][:shows][0][:info]).to include('Four Lights')
+    end
     it 'assigns @shows'
   end
 
