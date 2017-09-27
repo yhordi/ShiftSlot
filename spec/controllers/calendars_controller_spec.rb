55fd0d7c75e7225f77d59d0f1386a8624a00cef7
Jordan Kamin <jordanakamin@gmail.com>
2017-09-27 13:41:38 -0700

    bookable at 100% coverage

--- spec/controllers/calendars_controller_spec.rb
+++ spec/controllers/calendars_controller_spec.rb
@@ -6,24 +6,39 @@ RSpec.describe CalendarsController, type: :controller do
     sign_in user
   end
   describe '#sync' do
-    let(:get_sync) {}
-    let(:resp_double) { double(parsed_response: fake_response)}
-    before(:each) do
-      allow(HTTParty).to receive(:get).and_return(resp_double)
-      get :sync, params: {token: 'HAM'}
+    describe "on success" do
+      let(:get_sync) {}
+      let(:resp_double) { double(parsed_response: fake_response)}
+      before(:each) do
+        allow(HTTParty).to receive(:get).and_return(resp_double)
+        get :sync, params: {token: 'HAM'}
+      end
+      it 'responds with a 200' do
+        expect(response.status).to eq(200)
+      end
+      it 'renders the sync template' do
+        expect(response).to render_template('calendars/sync')
+      end
+      it 'assigns @google_shows to a hash containing imported values' do
+        expect(assigns[:google_shows][:shows][0][:info]).to include('Four Lights')
+      end
+      it 'assigns @shows' do
+        expect(assigns[:shows]).to eq(Show.all)
+      end
     end
-    it 'responds with a 200' do
-      expect(response.status).to eq(200)
+    describe 'on failure' do
+      let(:resp_double) { double(parsed_response: fake_response)}
+      before(:each) do
+        allow(HTTParty).to receive(:get).and_return(resp_double)
+      end
+      it 'assigns @google_shows with a conflicts key containing conflicting shows' do
+        FactoryGirl.create(:show, info: "Vic- Four Lights, Coyote Bred, Ol' Doris, The Subjunctives", start: DateTime.parse('2017-10-03T09:00:00-07:00'))
+        get :sync, params: {token: 'HAM'}
+        p assigns[:google_shows]
+        expect(assigns[:google_shows][:conflicts][0][:info]).to include('Four Lights')
+      end
     end
-    it 'renders the sync template' do
-      expect(response).to render_template('calendars/sync')
-    end
-    it 'assigns @google_shows to a hash containing imported values' do
-      expect(assigns[:google_shows][:shows][0][:info]).to include('Four Lights')
-    end
-    it 'assigns @shows' do
-      expect(assigns[:shows]).to eq(Show.all)
-    end
+
   end
 
   describe '#create' do
