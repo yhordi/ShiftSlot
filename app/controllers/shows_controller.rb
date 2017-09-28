a334a94c410af0cf9b024d3df19b1977e106775f
Jordan Kamin <jordanakamin@gmail.com>
2017-09-28 15:46:43 -0700

    removes antiquated Oauth code form shows_controller

--- app/controllers/shows_controller.rb
+++ app/controllers/shows_controller.rb
@@ -9,22 +9,4 @@ class ShowsController < ApplicationController
     render :show
   end
 
-  def callback
-  end
-
-  def redirect
-  end
-
-  private
-
-  def client_options
-    {
-      client_id: ENV['CLIENT_ID'],
-      client_secret: ENV['CLIENT_SECRET'],
-      authorization_uri: 'https://accounts.google.com/o/oauth2/auth',
-      token_credential_uri: 'https://accounts.google.com/o/oauth2/token',
-      scope: Google::Apis::CalendarV3::AUTH_CALENDAR,
-      redirect_uri: callback_url
-    }
-  end
 end
