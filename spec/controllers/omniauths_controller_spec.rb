edf141c15e554421f9e06883af04b39a5666152d
Jordan Kamin <jordanakamin@gmail.com>
2017-09-28 12:25:28 -0700

    adds in modern stubbing syntax

--- spec/controllers/omniauths_controller_spec.rb
+++ spec/controllers/omniauths_controller_spec.rb
@@ -1,12 +1,28 @@
 require 'rails_helper'
 require_relative '../support/response'
 RSpec.describe OmniauthsController, type: :controller do
+  before(:each) do
+    allow(ENV).to receive(:[]).with('CAL_CLIENT_ID').and_return('bobobobobo')
+    allow(ENV).to receive(:[]).with('CLIENT_SECRET').and_return('Big Dang Secret™')
+    allow(ENV).to receive(:[]).with('PROJECT_ID').and_return('Big Dang Project')
+  end
   describe '/redirect' do
-    it 'responds with a status of 302'
+    let(:resp_double) { double(parsed_response: fake_response)}
+    before(:each) do
+      allow(HTTParty).to receive(:post).and_return(resp_double)
+    end
+    it 'responds with a status of 302' do
+      get :redirect
+      expect(response.status).to eq(302)
+    end
     it 'sends a token in params'
   end
   describe '/callback' do
-    it 'responds with a status of 302'
+
+    it 'responds with a status of 302' do
+      get :callback
+      expect(response.status).to eq(302)
+    end
     it 'redirects to a url based on environment variables'
   end
 end
