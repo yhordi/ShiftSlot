51bf6be7f7cadb67170a22307d94ed3e55e6b745
Jordan Kamin <jordanakamin@gmail.com>
2017-10-24 11:20:23 -0700

    adds test for OrganizationsController#new

--- spec/controllers/organization_controller_spec.rb
+++ spec/controllers/organization_controller_spec.rb
@@ -4,21 +4,22 @@ RSpec.describe OrganizationsController, type: :controller do
   let(:org) { FactoryGirl.create :organization }
   describe '#new' do
     it 'assigns the @org instance variable to a new organization' do
+      get :new
+      expect(assigns[:org]).to be_a_new(Organization)
     end
   end
   describe '#create' do
     it 'assigns the @org instance variable to a new organization' do
-      post :create,
     end
     it 'responds with a status of 302' do
     end
-    context 'on success' do
-      it 'saves an org to the database'
-      it 'redirects to the user signup page'
-    end
-    context 'on fail' do
-      it 'sends errors back through a flash message'
-      it 'redirects to the new organization page'
-    end
+      context 'on success' do
+        it 'saves an org to the database'
+        it 'redirects to the user signup page'
+      end
+      context 'on fail' do
+        it 'sends errors back through a flash message'
+        it 'redirects to the new organization page'
+      end
   end
 end
