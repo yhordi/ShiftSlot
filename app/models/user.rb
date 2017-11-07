e3131bf4ec82ae067c9d215909240fd43f718f26
Jordan Kamin <jordanakamin@gmail.com>
2017-11-07 13:48:01 -0800

    adds unbiqueness validator for user email

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,6 +9,7 @@ class User < ApplicationRecord
   has_many :jobs, through: :authorized_jobs
   validates_presence_of :name, :email
   validates :name, length: { minimum: 3 }
+  validates_uniqueness_of :email
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -14,6 +14,13 @@ RSpec.describe User, type: :model do
   describe 'validations' do
     it { is_expected.to validate_presence_of :email }
     it { is_expected.to validate_length_of(:name).is_at_least(3) }
+    it 'is valid with a unique email' do
+      expect(user).to be_valid
+    end
+    it 'is invalid without a unique email' do
+      new_user.email = user.email
+      expect(new_user).to_not be_valid
+    end
   end
 
   describe 'associations' do
