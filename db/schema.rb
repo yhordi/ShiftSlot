105ef8c8e736bff696433604f4fb5e1582751abe
Jordan Kamin <jordanakamin@gmail.com>
2017-11-08 12:24:12 -0800

    removes organization id from user

--- db/schema.rb
+++ db/schema.rb
@@ -10,17 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171107215702) do
+ActiveRecord::Schema.define(version: 20171108202227) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "assignments", force: :cascade do |t|
-    t.integer  "user_id",         null: false
-    t.integer  "organization_id", null: false
-    t.datetime "created_at",      null: false
-    t.datetime "updated_at",      null: false
-    t.boolean  "admin"
+    t.integer  "user_id",                         null: false
+    t.integer  "organization_id",                 null: false
+    t.datetime "created_at",                      null: false
+    t.datetime "updated_at",                      null: false
+    t.boolean  "admin",           default: false
     t.index ["organization_id"], name: "index_assignments_on_organization_id", using: :btree
     t.index ["user_id"], name: "index_assignments_on_user_id", using: :btree
   end
@@ -105,9 +105,7 @@ ActiveRecord::Schema.define(version: 20171107215702) do
     t.string   "name"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
-    t.integer  "organization_id"
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
-    t.index ["organization_id"], name: "index_users_on_organization_id", using: :btree
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,32 +1,33 @@
-require 'faker'
+# require 'faker'
 org = Organization.create(name: 'SoundGig Presents')
-org.venues << Venue.create!(name: 'Victory Lounge', location: 'Seattle, WA')
-org.venues << Venue.create!(name: 'Black Lodge', location: 'Seattle, WA')
-org.venues << Venue.create!(name: 'Lucky Liquor', location: 'Tukwila, WA')
-20.times do
-  s = Show.new(doors: Faker::Date.forward)
-  s.organization = org
-  s.start = s.doors + 30.minutes
-  s.show_end = s.doors + 4.hours
-  s.venue_id = [1,2,3].sample
-  s.headliner = Faker::RockBand.name
-  s.info = Faker::Lorem.sentence
-  s.save!
-end
-20.times do
-  u = User.new(name: Faker::Name.name, email: Faker::Internet.email, password: Faker::Internet.password)
-  u.organizations << org
-end
-
-Venue.all.each do |venue|
-  Job.create(title: 'Security', venue: venue)
-  Job.create(title: 'Bar', venue: venue)
-  Job.create(title: 'Door', venue: venue)
-  Job.create(title: 'Sound', venue: venue)
-end
-
-User.all.each { |user| user.jobs << Job.find(3)}
+# org.venues << Venue.create!(name: 'Victory Lounge', location: 'Seattle, WA')
+# org.venues << Venue.create!(name: 'Black Lodge', location: 'Seattle, WA')
+# org.venues << Venue.create!(name: 'Lucky Liquor', location: 'Tukwila, WA')
+# 20.times do
+#   s = Show.new(doors: Faker::Date.forward)
+#   s.organization = org
+#   s.start = s.doors + 30.minutes
+#   s.show_end = s.doors + 4.hours
+#   s.venue_id = [1,2,3].sample
+#   s.headliner = Faker::RockBand.name
+#   s.info = Faker::Lorem.sentence
+#   s.save!
+# end
+# 20.times do
+#   u = User.new(name: Faker::Name.name, email: Faker::Internet.email, password: Faker::Internet.password)
+#   u.organizations << org
+# end
+#
+# Venue.all.each do |venue|
+#   Job.create(title: 'Security', venue: venue)
+#   Job.create(title: 'Bar', venue: venue)
+#   Job.create(title: 'Door', venue: venue)
+#   Job.create(title: 'Sound', venue: venue)
+# end
+#
+# User.all.each { |user| user.jobs << Job.find(3)}
 
 a = User.new(name: 'admin', email: 'admin@email.com', password: ENV['PASSWORD'])
 a.organizations << org
 a.assignments.first.admin = true
+a.save!
