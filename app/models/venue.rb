e3e77b03d589f7372f9fdb488b51f29646a2eaa0
Jordan Kamin <jordanakamin@gmail.com>
2017-11-28 12:56:38 -0800

    adds #seed_jobs to venue

--- app/models/venue.rb
+++ app/models/venue.rb
@@ -4,8 +4,14 @@ class Venue < ApplicationRecord
   has_many :jobs, dependent: :destroy
   has_many :partnerships
   has_many :organizations, through: :partnerships
+  after_create :seed_jobs
 
   protected
+
+  def seed_jobs
+    self.jobs << Job.create(title: 'volunteer')
+  end
+
 
   def self.abbreviations
     Venue.all.map do |venue|
