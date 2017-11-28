6a76def4fd40ce0f1ec5e1c99413f68f628a4597
Jordan Kamin <jordanakamin@gmail.com>
2017-11-28 13:14:58 -0800

    adds #authorize_volunteer to Organization

--- app/models/organization.rb
+++ app/models/organization.rb
@@ -1,16 +1,32 @@
 class Organization < ApplicationRecord
   has_many :assignments
-  has_many :users, through: :assignments
+  has_many :users, through: :assignments, after_add: :authorize_volunteer
   has_many :partnerships, dependent: :destroy
   has_many :venues, through: :partnerships, dependent: :destroy
   has_many :shows, through: :venues, dependent: :destroy
   validates_presence_of :name
   validates_uniqueness_of :name
 
+
   def upcoming_shows(index = 4)
     upcoming = self.shows.order(:start).select do |show|
       show.start > Time.now
     end
     upcoming[0..index]
+  end
+
+  private
+
+  def authorize_volunteer(worker)
+    volunteer_positions.each do |job|
+      worker.jobs << job
+    end
+  end
+
+  def volunteer_positions
+    Job.all.select do |job|
+      job.venue.organizations.include?(self) && job.title == 'volunteer'
+    end
+    # self.venues.jobs.where(title: 'volunteer')
   end
 end
