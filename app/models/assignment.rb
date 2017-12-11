687d80f8c6174673ba462cce3f5b3d389cfadfcd
Jordan Kamin <jordanakamin@gmail.com>
2017-12-11 12:12:26 -0800

    adds check in org dashboard for authorized users

--- app/models/assignment.rb
+++ app/models/assignment.rb
@@ -5,4 +5,8 @@ class Assignment < ApplicationRecord
   def self.find_match(user_id:, organization_id:)
     self.where(user_id: user_id, organization_id: organization_id)[0]
   end
+
+  def authorized?
+    self.authorized
+  end
 end
