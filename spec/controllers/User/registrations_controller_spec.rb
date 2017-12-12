3811b55af4b861d71ca59087c8a60fd4604abfa4
Jordan Kamin <jordanakamin@gmail.com>
2017-12-12 11:56:50 -0800

    adds tests for User::RegistrationsController#create when no org_id is passed

--- spec/controllers/User/registrations_controller_spec.rb
+++ spec/controllers/User/registrations_controller_spec.rb
@@ -12,5 +12,20 @@ RSpec.describe User::RegistrationsController, type: :controller do
       expect(assigns(:org)).to eq(org)
     end
   end
+  describe '#create' do
+    context 'when passed an organization_id' do
+      it 'creates an assignment'
+      it 'sets the new user as the admin for the new organization'
+      it 'redirects to the organization_path'
+    end
+    it 'responds with a status of 302' do
+      post :create, params: {user: user_attrs}
+      expect(response.status).to eq 302
+    end
+    it 'redirects to the new_user_assignment_path' do
+      post :create, params: {user: user_attrs}
+      expect(response).to redirect_to new_user_assignment_path(User.last)
+    end
+  end
 
 end
