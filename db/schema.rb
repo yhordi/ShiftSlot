6193b2da7bcc7959f35088081b977462d9bcf8a3
Jordan Kamin <jordanakamin@gmail.com>
2017-12-14 12:35:01 -0800

    adds hooks field to venues

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171214203009) do
+ActiveRecord::Schema.define(version: 20171214203344) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -117,6 +117,7 @@ ActiveRecord::Schema.define(version: 20171214203009) do
     t.string   "location"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string   "hooks"
   end
 
 end
