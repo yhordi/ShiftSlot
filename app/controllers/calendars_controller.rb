8caa81fa0dfbf9164260b4a6f9fead68aa5074fd
Jordan Kamin <jordanakamin@gmail.com>
2018-02-05 15:10:30 -0800

    adds panels to sync page

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -1,18 +1,19 @@
 class CalendarsController < ApplicationController
   include JSON
   include Bookable
+
   def sync
     headers = "Bearer #{params[:token]}"
     @org = Organization.find(params[:organization_id])
-
     base = "https://www.googleapis.com/calendar/v3/calendars/#{@org.gcal_id}/events?"
     data = URI.encode_www_form(key: ENV['CAL_KEY'], timeMin: params["state"]["timeMin"], timeMax: params["state"]["timeMax"])
     url = "#{base}?#{data}"
     req = HTTParty.get(url, headers: {"Authorization" => headers})
     @google_shows = build(req.parsed_response)
-    @shows = Show.all
+    @google_shows[:shows] = @google_shows[:shows].sort_by { |s| s.start }
     render 'sync'
   end
+
   def new
     @org = Organization.find(params[:organization_id])
     render :new
